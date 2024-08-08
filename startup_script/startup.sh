4577b2efd43eaa2913deabb1fcbbe8ca9fa84a49
Morrie Swerlick <morrie@seesaw.me>
2024-08-08 17:56:12 -0400

    Add SSH tunnel to startup script

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo "Running sample startup script."
+echo 'Running sample startup script.'
+
+chmod 400 ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/rds_proxy_dev.pem;
+
+ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/rds_proxy_dev.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
