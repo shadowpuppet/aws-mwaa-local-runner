1bf5df9b882f2f06c707cdc8df7b6be17a1dd255
Morrie Swerlick <morrie@seesaw.me>
2024-08-08 18:27:24 -0400

    renamed file

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -2,6 +2,6 @@
 
 echo 'Running sample startup script.'
 
-chmod 400 ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/rds_proxy_dev.pem;
+chmod 400 ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/ssh_host.pem;
 
-ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/rds_proxy_dev.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
+ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/ssh_host.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
