799ffd53f56a040fe658f4d6cd57448b219f0882
Ishita Katyal <ishita.katyal@beehyv.com>
2025-02-03 18:13:11 +0530

    Updated echo statements for ENV-STAGE and prefix in .envlocal-runner

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Running startup script."
-echo "Environment stage set to:" $ENVIRONMENT_STAGE
+echo "Airflow Environment is:" $ENVIRONMENT_STAGE
 
 ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/ssh_host.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
