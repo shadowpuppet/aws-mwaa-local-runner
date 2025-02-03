f21c37c14b5db218eff2641e4c748136dbb782f4
atish-beehyv <atish.sisodia@seesaw.me>
2025-02-03 16:12:06 +0530

    pausing all active dags on every start.

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -3,3 +3,4 @@
 echo 'Running sample startup script.'
 
 ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/ssh_host.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
+airflow dags pause --treat-dag-id-as-regex '.*' -y
