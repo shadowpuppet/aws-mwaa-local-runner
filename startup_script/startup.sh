4901387b138f26556ab337adc7f99a7da85bd496
atish-beehyv <atish.sisodia@seesaw.me>
2025-02-13 13:12:43 +0530

    used startup.sh for exporting env variable

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "Running startup script."
+
+export ENVIRONMENT_STAGE="local"
 echo "Airflow Environment is:" $ENVIRONMENT_STAGE
 
 ssh -4 -i ${AIRFLOW_LOCAL_CONNECTIONS_DIR:-${PWD}}/files/keys/ssh_host.pem -fNT -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -L 5432:${RDS_HOST}:5432 ${SSH_ADDRESS}
